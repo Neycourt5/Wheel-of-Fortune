8c0d009fa47d440b15fdf5a4825328d24499c083
Neycourt5 <Acourtney1990@gmail.com>
2021-04-24 14:10:07 +0900

    Having trouble with turns

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Player names in the game are
+//Player names in the game
 const p1Name = document.querySelector("#p1namedisplay")
 const p2Name = document.querySelector("#p2namedisplay")
 const p3Name = document.querySelector("#p3namedisplay")
@@ -7,6 +7,9 @@ const p3Name = document.querySelector("#p3namedisplay")
 const p1Score = document.querySelector("#p1score")
 const p2Score = document.querySelector("#p2score")
 const p3Score = document.querySelector("#p3score")
+
+//select player turn notifier
+const turnDisp = document.querySelector(".turn")
 
 //modal popup elments
 const modalBtn = document.querySelector("#submitnames")
@@ -26,6 +29,16 @@ const playersContainer = document.querySelector(".playerscontainer")
 const playableSquares = document.querySelectorAll('.playable')
 
 
+
+//score variables?
+
+//declare array of player objects
+const players = [{
+    name: p1Name.textContent,
+    score: p1Score
+}, { name: p2Name.textContent, score: p2Score.textContent }, { name: p3Name.textContent, score: p3Score.textContent }]
+
+
 //Beginning pop up to choose team names
 modalBtn.addEventListener("click", (e) => {
     modal.classList.add("none")
@@ -35,92 +48,114 @@ modalBtn.addEventListener("click", (e) => {
     p1Name.textContent = p1NameInput.value
     p2Name.textContent = p2NameInput.value
     p3Name.textContent = p3NameInput.value
+    turnDisp.textContent = `${p1Name.textContent} turn`
     //newRound()
+    //assign names
+    players[0].name = p1Name.textContent
+    players[1].name = p2Name.textContent
+    players[2].name = p3Name.textContent
+    //start game
     game()
 })
 
+const checkForLetter = (e, gamePhrase, player) => {
+    //select the white letter squares
+    const letterSquares = document.querySelectorAll('.lettersquare')
+    let letters = gamePhrase.split("")
+    let wrongGuess = false
+    letters = letters.filter(function (str) {
+        return /\S/.test(str);
+    });
+    //search white spaces for letters and replace add letters into squares
+    if (letters.includes(e.key.toUpperCase()) || letters.includes(e.key)) {
+        for (let i = 0; i < letters.length; i++) {
+            if (letters[i] === e.key.toUpperCase()) {
+                letterSquares[i].textContent = e.key.toUpperCase();
+            }
+            else if (letters[i] === e.key) {
+                letterSquares[i].textContent = e.key
+            }
+        }
+    }
+}
 
+const populateSquares = (phrase) => {
+    //make it start on the start square on the second row if its not too long
+    let words = phrase.split(" ")
+    let wordLetters = phrase.split("")
+    let position = 10
+    let remainSpace = 12
+    let repositionCount = 0
+    console.log(phrase)
 
+    //populate the board with white squares
+    for (word of words) {
+        //repositions the word to a new line if it wont fit
+        if (word.trim().length > remainSpace) {
+            position += remainSpace
+            remainSpace = 12
+            repositionCount++
+        }
+        for (let i = 0; i < word.length; i++) {
+            if (word[i].trim().match(/[a-z]/i)) {
 
+                playableSquares[position].classList.add('lettersquare')
+            }
+            position++
+            remainSpace--
+        }
+        //end of the word so this establishes a space between words
+        position++
+        remainSpace--
+    }
+}
 
-//function for populating the squares and turning them white
-let testPhrase = "My name is Alex"
-//testPhrase = testPhrase.split('').filter(char => /[a-zA-Z]/.test(char));
-//i need to find a way to filter commas and periods out of the string so we dont create unneeded spaces on the board
 
 //varables to determinte the state of the game (3 rounds before game over)
 let gameOver = false;
 let roundNum = 1;
 
 
-// while (gameOver === false) {
+//Need to make async maybe
+//If function resolves, keep it on the same player and let them guess again (and spin when i get to that)
+//Otherwise if it doesnt resolve, move to the next player
+const playerTurn = (gamePhrase, player) => {
+    turnDisp.textContent = `${player.name}'s turn`
+    //Spin wheel here
+    let wrongGuess = false
+    //If wheel lands on money start a loop that goes until they guess a wrong letter
+    //else, move to next {player
 
-// }
-
+    //listen for key presses
+    document.addEventListener('keyup', (e) => { checkForLetter(e, gamePhrase, player) })
+}
 
 const game = () => {
     //populates the board with white squares
-    const populateSquares = (phrase) => {
-        //make it start on the start square on the second row if its not too long
-        let words = phrase.split(" ")
-        let wordLetters = phrase.split("")
-        let position = 10
-        let remainSpace = 12
-        let repositionCount = 0
-        console.log(phrase)
-        //populate the board with white squares
-        for (word of words) {
-            //repositions the word to a new line if it wont fit
-            if (word.trim().length > remainSpace) {
-                position += remainSpace
-                remainSpace = 12
-                repositionCount++
-            }
-            for (let i = 0; i < word.length; i++) {
-                if (word[i].trim().match(/[a-z]/i)) {
-                    playableSquares[position].classList.add('lettersquare')
-                }
-                position++
-                remainSpace--
-            }
-            //end of the word so this establishes a space between words
-            position++
-            remainSpace--
-        }
-    }
 
-    //Make game here
+
+    //Populate squares with the phrase
     const phrases = ["My name is Alex", "I like to play baseball", "This is bullshit", "Sally sold sea shells"]
     let gamePhrase = phrases[(Math.floor(Math.random() * phrases.length))]
     populateSquares(gamePhrase)
+
+    // while (!gameOver) {
+
+
+    // }
+
+
     // //new game/round function
     // const newRound = () => {
     //     let gamePhrase = phrases[(Math.floor(Math.random() * phrases.length))]
     //     populateSquares(gamePhrase)
     // }
 
-    //select the white letter squares
-    const letterSquares = document.querySelectorAll('.lettersquare')
+
+    //Function for the player turn
+
+    playerTurn(gamePhrase, players[0])
 
 
-
-    //listen for key presses
-    document.addEventListener('keyup', (e) => {
-        let letters = gamePhrase.split("")
-        letters = letters.filter(function (str) {
-            return /\S/.test(str);
-        });
-        //search white spaces for letters and replace add letters into squares
-        if (letters.includes(e.key.toUpperCase()) || letters.includes(e.key)) {
-            for (let i = 0; i < letters.length; i++) {
-                if (letters[i] === e.key.toUpperCase()) {
-                    letterSquares[i].textContent = e.key.toUpperCase();
-                }
-                else if (letters[i] === e.key) {
-                    letterSquares[i].textContent = e.key
-                }
-            }
-        }
-    })
 }
 
